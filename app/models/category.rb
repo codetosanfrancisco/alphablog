67273e998bfdb57420e24acd8358707b1e262f71
Voon Shun Zhi <voonshunzhi@gmail.com>
2018-07-15 10:21:15 +0000

    Done

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+    has_many :article_categories #has many column in intermediary table
+    has_many :articles,through: :article_categories #has many column articles table connected through article_categories table
     validates :name,presence:true,length:{maximum:20,minimum:5},uniqueness:{case_sensitive:false}
 end
